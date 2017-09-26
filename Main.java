75bcb32384378f4ad3fca7c10f68e60dd42d0dd7
ssyydd99 <32283911+ssyydd99@users.noreply.github.com>
2017-09-26 09:57:33 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int blue = 4;
+        int purple = 2;
+        int red = 8;
+
+        purple = red + blue;
+        System.out.println(purple);
+    }
+}
